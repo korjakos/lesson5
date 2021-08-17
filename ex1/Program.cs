bafaf7ba06a20127d7f72f7442811ee6bfe9e117
Korjakos <nurumov.ol@mail.ru>
2021-08-17 23:32:41 +0600

    Добавьте файлы проекта.

---
+++ ex1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ex1
+{
+    class Program
+    {
+        //попытки
+        static string RightTry(int x)
+        {
+            string s = "";
+            if (x % 10 == 1 && x != 11) s += " попытка";
+            else
+                if ((x >= 2 && x <= 4) || (x >= 22 && x <= 24) || (x >= 32 && x <= 34) || (x > 41 && x < 45)) s += " попытки";
+            else                   
+                    if ((x == 11) || (x >= 5 && x <= 20) || (x >= 25 && x <= 30) || (x >= 35 && x < 41) || (x > 44 && x < 51)) s += " попыток";
+            return s;
+        }
+
+        //Проверка правильности ввода
+        static bool CheckLogin(string login)
+        {
+            int length = login.Length;
+            if (length >= 2 && length <= 10)
+            {
+                bool check = true;
+                char letter = login[0];
+                if (Char.IsDigit(letter))
+                    return false;
+                for (int i = 1; i < length; i++)
+                {
+                    letter = login[i];
+                    if (!(Char.IsDigit(letter) || IsLatinLetter(letter)))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    return true;
+            }
+            return false;
+        }
+
+        //Проверка правильности ввода через регулярные выражения
+        static bool CheckLoginReg(string login)
+        {
+            char letter = login[0];
+            if (Char.IsDigit(letter))
+                return false;
+            if (!Regex.IsMatch(login, @"^[a-zA-Z0-9]+${2,10}"))
+                return false;
+            return true;
+        }
+
+        //проверка латиницы
+        private static bool IsLatinLetter(char letter)
+        {
+            int num = letter;
+            if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа проверки корректности логина.");
+            int AmountOfTries = 3;
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+
+                if (CheckLogin(login) && CheckLoginReg(login))
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    AmountOfTries--;
+                    Console.WriteLine("Неверный ввод логина. \nДолжны быть соблюдены следующие условия:"
+                        + "\nдлина строки 2 до 10 символов;"
+                        + "\nбуквы только латинского алфавита или цифры;"
+                        + "\nцифра не может быть первой."
+                        + Environment.NewLine + "У Вас осталось " + AmountOfTries + RightTry(AmountOfTries));
+                }
+
+            } while (AmountOfTries > 0);
+
+            Console.WriteLine("Логин корректен!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+
+namespace ex2
+{
+    static class Message
+    {
+        static public string text;
+
+        static Message()
+        {
+            text = "Разнообразный и богатый опыт консультация с широким активом обеспечивает широкому кругу.  ";
+        }
+
+        //а) Вывести только те слова сообщения, которые содержат не более n букв.
+        static public void GetWordsByLength(int len)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            Console.WriteLine("Вывод слов, длинной не более " + len + ": " );
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write(word + " ");
+            }
+        }
+
+        //в) Найти самое длинное слово сообщения.
+        static public string FindMaxLengthWord()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+            return maxWord;
+        }
+
+        //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        static public StringBuilder GetLongWordsString()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.FindMaxLengthWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word.ToLower() + " ");
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nИмеется следующий текст: \n" + Message.text);
+
+            Console.WriteLine("\nВыведем слова текста, которые содержат не более 5 букв:");
+            Message.GetWordsByLength(5);
+
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+
+            Console.WriteLine("\nСформированная строка StringBuilder из самых длинных слов сообщения: \n" + Message.GetLongWordsString());
+
+            Console.ReadKey();
+        }
+    }
+}
